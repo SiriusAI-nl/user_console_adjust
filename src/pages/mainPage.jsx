4afe79aefcd5ff892871a814dff0df89cfc0286f
Faizan Dawood <113394135+FaixySaati@users.noreply.github.com>
2025-01-30 23:56:43 +0500

    working

--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,18 +1,34 @@
 import React, { useEffect, useRef, useState } from "react";
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion, AnimatePresence } from "framer-motion";
 import SearchBox from "@/components/searchBox";
 import EditSearch from "@/components/editSearch";
 import Message from "@/components/Message";
 import Starting from "@/components/starting";
 import { PiPaperPlaneTiltFill } from "react-icons/pi";
 import { FaCircleStop } from "react-icons/fa6";
+import axios from "axios";
 
 const MainPage = ({ setMenuOpen, setIsBtn }) => {
   const [isPlanning, setIsPlanning] = useState(false);
   const [newtext, setNewtext] = useState("");
   const [isAIType, setIsAIType] = useState(false);
   const [messages, setMessages] = useState([]);
-  const [isSearchPlan, setIsSearchPlan] = useState(false)
+  const [isSearchPlan, setIsSearchPlan] = useState(false);
+  const [isType, setIsType] = useState(false);
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const fetchMessages = async () => {
+    let { data } = await axios.get(`${API_URL}/api/chat`);
+    
+  };
+  useEffect(() => {
+    // fetchMessages();
+    // fetchPlans();
+  }, []);
+  const fetchPlans = async () => {
+    let { data } = await axios.get(`${API_URL}/api/plan`);
+    console.log(data);
+  };
 
   const messagesEndRef = useRef(null);
 
@@ -22,48 +38,62 @@ const MainPage = ({ setMenuOpen, setIsBtn }) => {
       ...prevMessages,
       { message: "Hello! How can I help you?", sender: "ai" },
     ]);
-    setIsSearchPlan(true)
+    setIsSearchPlan(true);
   };
 
-  const handleMessage = () => {
+
+  const handleMessage = async () => {
+    if (isType) return;
     if (newtext.trim() === "") return;
     setMessages((prevMessages) => [
       ...prevMessages,
       { message: newtext, sender: "user" },
     ]);
     setIsAIType(true);
-    setTimeout(handleAIMessage, 1000);
+    setIsType(true);
     setNewtext("");
+    try {
+      let response = await axios.post(`${API_URL}/api/chat`, {
+        message: newtext,
+      })
+          
+    } catch (error) {
+      alert(error)
+      console.log(error,"data");
+    }
+    setIsAIType(false);
+    setIsType(false);
   };
+
 
   useEffect(() => {
     if (messagesEndRef.current) {
       messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
     }
-  }, [messages]);
+  }, [messages, isPlanning]);
 
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-  const [isMedium, setIsMedium] = useState(false)
+  const [isMedium, setIsMedium] = useState(false);
 
   useEffect(() => {
-          const handleResize = () => {
-              setWindowWidth(window.innerWidth);
-          };
-  
-          window.addEventListener('resize', handleResize);
-  
-          return () => {
-              window.removeEventListener('resize', handleResize);
-          };
-      }, []);
-  
-      useEffect(() => {
-          if (windowWidth < 768) {
-            setIsMedium(true)
-          } else {
-            setIsMedium(false)
-          }
-      }, [windowWidth, setMenuOpen]);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (windowWidth < 768) {
+      setIsMedium(true);
+    } else {
+      setIsMedium(false);
+    }
+  }, [windowWidth, setMenuOpen]);
 
   return (
     <div className="sm:static relative flex justify-center px-5 gap-x-5 h-[89vh]">
@@ -71,7 +101,7 @@ const MainPage = ({ setMenuOpen, setIsBtn }) => {
         className={`flex flex-col justify-between ${
           isPlanning ? "w-[35%]" : "max-w-[713px]"
         } mx-auto w-full h-full`}
-        animate={{ width: (isPlanning && !isMedium) ? "35%" : "100%" }}
+        animate={{ width: isPlanning && !isMedium ? "35%" : "100%" }}
         transition={{ duration: 0.3 }}
       >
         <div className="mb-5 w-full h-[90%] overflow-y-auto pt-[10px]">
@@ -82,14 +112,37 @@ const MainPage = ({ setMenuOpen, setIsBtn }) => {
           ) : (
             <div className="h-[60%] text-center w-full flex flex-col justify-center items-center mt-20">
               <h1 className="sm:text-[40px] text-[25px] font-[600] text-[#fafafa] flex ">
-                Welcome to<pre className="text-[#a854f7]"> SiriusAI</pre>
+                Welcome to
+                <span className="text-purple-500">&nbsp;SiriusAI</span>
               </h1>
               <p className="mt-[5px] text-[16px] font-[600] text-[#6E7079]">
                 Start a conversation to generate marketing research insights
               </p>
             </div>
           )}
-          {isSearchPlan && <EditSearch setIsPlanning={setIsPlanning} setMenuOpen={setMenuOpen}/>}
+          {isSearchPlan && (
+            <EditSearch
+              setIsPlanning={setIsPlanning}
+              setMenuOpen={setMenuOpen}
+            />
+          )}
+          {isMedium && (
+            <AnimatePresence>
+              {isPlanning && (
+                <motion.div
+                  initial={{ width: 0, opacity: 0 }}
+                  animate={{ width: "100%", opacity: 1 }}
+                  exit={{ width: 0, opacity: 0 }}
+                  transition={{ duration: 0.3 }}
+                >
+                  <Starting
+                    isPlanning={isPlanning}
+                    setIsPlanning={setIsPlanning}
+                  />
+                </motion.div>
+              )}
+            </AnimatePresence>
+          )}
           <div ref={messagesEndRef} />
           {isAIType && (
             <div
@@ -101,7 +154,13 @@ const MainPage = ({ setMenuOpen, setIsBtn }) => {
             </div>
           )}
         </div>
-        <div className="sm:mb-0 mb-3 dark:bg-[#3D3D3D] bg-[#1F2937] border border-gray-700 hover:border-purple-500 w-full rounded-[10px] flex items-center gap-2 px-4 py-2 max-h-[57px] text-gray-300">
+        <form
+          className="sm:mb-0 mb-3 dark:bg-[#3D3D3D] bg-[#1F2937] border border-gray-700 hover:border-purple-500 w-full rounded-[10px] flex items-center gap-2 px-4 py-2 max-h-[57px] text-gray-300"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleMessage();
+          }} // Ensure form submits on enter key press
+        >
           <textarea
             type="text"
             placeholder="Ask a follow-up question..."
@@ -109,36 +168,51 @@ const MainPage = ({ setMenuOpen, setIsBtn }) => {
             value={newtext}
             rows={1}
             onChange={(e) => setNewtext(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" && !e.shiftKey) {
+                e.preventDefault();
+                handleMessage(e);
+              }
+            }}
           ></textarea>
           <button
+            type="submit"
             className={`flex justify-center items-center p-2 rounded-md transition-all duration-300 ${
               !newtext.trim()
                 ? "text-gray-400 cursor-default"
                 : "text-[#340061] hover:bg-[#1F2937] active:bg-[#E9D5FF]"
             } bg-transparent`}
-            onClick={handleMessage}
-            disabled={!newtext.trim()}
+            onClick={() => {
+              if (isAIType) {
+                handleMessage();
+              } else {
+                setIsAIType(false);
+              }
+            }}
+            disabled={isType || !newtext.trim()}
           >
             {isAIType ? (
               <FaCircleStop className="text-2xl" />
             ) : (
-              <PiPaperPlaneTiltFill className="text-2xl text-gray-300"/>
+              <PiPaperPlaneTiltFill className="text-2xl text-gray-300" />
             )}
           </button>
-        </div>
+        </form>
       </motion.div>
-      {!isMedium && <AnimatePresence className="md:flex hidden">
-        {isPlanning && (
-          <motion.div
-            initial={{ width: 0, opacity: 0 }}
-            animate={{ width: "60%", opacity: 1 }}
-            exit={{ width: 0, opacity: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <Starting isPlanning={isPlanning} setIsPlanning={setIsPlanning} />
-          </motion.div>
-        )}
-      </AnimatePresence>}
+      {!isMedium && (
+        <AnimatePresence className="md:flex hidden">
+          {isPlanning && (
+            <motion.div
+              initial={{ width: 0, opacity: 0 }}
+              animate={{ width: "60%", opacity: 1 }}
+              exit={{ width: 0, opacity: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <Starting isPlanning={isPlanning} setIsPlanning={setIsPlanning} />
+            </motion.div>
+          )}
+        </AnimatePresence>
+      )}
     </div>
   );
 };
